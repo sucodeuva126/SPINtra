86e7d0ac55b905e54b19aa91a8bfa8b8df118b46
Gustavo <gus.henrique126@usp.br>
2026-03-26 15:26:17 -0300

    trying to close and open bvt com objetcs by brute force

--- bin/bvt_functions.py
+++ bin/bvt_functions.py
@@ -8,49 +8,79 @@ class BVT:
     self.current_temp = None
     self.threshold = threshold
 
-  def init_com_server(self): #COM objects are not thread safe #PROBLEM HERE
+#COM objects are not thread safe #PROBLEM HERE
+
+  def start(self, gas_flow, evaporator):
     self.emb = win.Dispatch("WinAcquisit.Embedding")
     self.emb.ShowWindow(self.emb.NORMAL)
     self.bvt_server = win.Dispatch("WinAcquisit.BVT")
     self.uti = win.Dispatch("WinAcquisit.Utilities")
-
-  def start(self, gas_flow, evaporator):
-    self.init_com_server()
     self.bvt_server.GasFlow(gas_flow)
     self.bvt_server.GasFlowOn(True)
     if evaporator:
       self.bvt_server.EvaporatorOn(True)
       self.bvt_server.EvaporatorPower(gas_flow)
     self.bvt_server.HeaterOn(True)
+    self.emb = None
+    self.bvt_server = None
+    self.uti = None
     return
 
   def set_point_and_start_ramp(self, temp):
-    self.init_com_server()
+    self.emb = win.Dispatch("WinAcquisit.Embedding")
+    self.emb.ShowWindow(self.emb.NORMAL)
+    self.bvt_server = win.Dispatch("WinAcquisit.BVT")
+    self.uti = win.Dispatch("WinAcquisit.Utilities")
     self.bvt_server.DesiredTemperature(temp)
     self.bvt_server.RampGO
+    self.emb = None
+    self.bvt_server = None
+    self.uti = None
     return 
 
   def autotune(self, switch):
-    self.init_com_server()
+    self.emb = win.Dispatch("WinAcquisit.Embedding")
+    self.emb.ShowWindow(self.emb.NORMAL)
+    self.bvt_server = win.Dispatch("WinAcquisit.BVT")
+    self.uti = win.Dispatch("WinAcquisit.Utilities")
     if switch == True:
       self.bvt_server.PIDTuneOn(True)
     if switch == False:
       self.bvt_server.PIDTuneOn(False)
+    self.emb = None
+    self.bvt_server = None
+    self.uti = None
     return
     
   def get_temperature(self):
-    self.init_com_server()
+    self.emb = win.Dispatch("WinAcquisit.Embedding")
+    self.emb.ShowWindow(self.emb.NORMAL)
+    self.bvt_server = win.Dispatch("WinAcquisit.BVT")
+    self.uti = win.Dispatch("WinAcquisit.Utilities")
     try:
         self.current_temp  = self.bvt_server.GetTemperature #saves the temperature read
+        self.emb = None
+        self.bvt_server = None
+        self.uti = None
     except Exception as e:
+        self.emb = None
+        self.bvt_server = None
+        self.uti = None
         print("ERROR READING BVT TEMPERATURES", e)
+    
 
   def check_temperature(self, temp): #thread function
-    self.init_com_server()
+    self.emb = win.Dispatch("WinAcquisit.Embedding")
+    self.emb.ShowWindow(self.emb.NORMAL)
+    self.bvt_server = win.Dispatch("WinAcquisit.BVT")
+    self.uti = win.Dispatch("WinAcquisit.Utilities")
     if self.current_temp is not None:
         if self.bvt_server.IsTemperatureOK: #verify if the mesured temperature is the desired temperature 
             self.isTemperatureReady = True
         else:
             self.isTemperatureReady = False
+    self.emb = None
+    self.bvt_server = None
+    self.uti = None
 
 
