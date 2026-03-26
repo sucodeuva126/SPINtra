1dbef226ca2ca1f57fdbf31fa525a2cd2ca5443f
Gustavo <gus.henrique126@usp.br>
2026-03-26 14:07:29 -0300

    testing with init_com_server

--- bin/bvt_functions.py
+++ bin/bvt_functions.py
@@ -7,7 +7,6 @@ class BVT:
     self.isTemperatureReady = False
     self.current_temp = None
     self.threshold = threshold
-    self._tls = threading.local()
 
   def init_com_server(self): #COM objects are not thread safe #PROBLEM HERE
     self.emb = win.Dispatch("WinAcquisit.Embedding")
@@ -17,39 +16,39 @@ class BVT:
 
   def start(self, gas_flow, evaporator):
     self.init_com_server()
-    self._tls.bvt_server.GasFlow(gas_flow)
-    self._tls.bvt_server.GasFlowOn(True)
+    self.bvt_server.GasFlow(gas_flow)
+    self.bvt_server.GasFlowOn(True)
     if evaporator:
-      self._tls.bvt_server.EvaporatorOn(True)
-      self._tls.bvt_server.EvaporatorPower(gas_flow)
-    self._tls.bvt_server.HeaterOn(True)
+      self.bvt_server.EvaporatorOn(True)
+      self.bvt_server.EvaporatorPower(gas_flow)
+    self.bvt_server.HeaterOn(True)
     return
 
   def set_point_and_start_ramp(self, temp):
     self.init_com_server()
-    self._tls.bvt_server.DesiredTemperature(temp)
-    self._tls.bvt_server.RampGO
+    self.bvt_server.DesiredTemperature(temp)
+    self.bvt_server.RampGO
     return 
 
   def autotune(self, switch):
     self.init_com_server()
     if switch == True:
-      self._tls.bvt_server.PIDTuneOn(True)
+      self.bvt_server.PIDTuneOn(True)
     if switch == False:
-      self._tls.bvt_server.PIDTuneOn(False)
+      self.bvt_server.PIDTuneOn(False)
     return
     
   def get_temperature(self):
     self.init_com_server()
     try:
-        self.current_temp  = self._tls.bvt_server.GetTemperature #saves the temperature read
+        self.current_temp  = self.bvt_server.GetTemperature #saves the temperature read
     except Exception as e:
-        print("ERROR READING BVT TEMPERATURES", e)
+        print("ERROR IN READING TEMPERATURES", e)
 
   def check_temperature(self, temp): #thread function
     self.init_com_server()
     if self.current_temp is not None:
-        if self._tls.bvt_server.IsTemperatureOK: #verify if the mesured temperature is the desired temperature 
+        if self.bvt_server.IsTemperatureOK: #verify if the mesured temperature is the desired temperature 
             self.isTemperatureReady = True
         else:
             self.isTemperatureReady = False
