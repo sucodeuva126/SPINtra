6d56326e3d04ceddad6724f6a1f05e4dc10181f4
Gustavo <gus.henrique126@usp.br>
2026-03-27 13:29:09 -0300

    removed ._tls and threadin.local()

--- bin/bvt_functions.py
+++ bin/bvt_functions.py
@@ -9,33 +9,32 @@ class BVT:
         self.isTemperatureReady = False
         self.current_temp = None
         self.threshold = threshold
-        self._tls = threading.local()
 
     #  inicializa COM UMA VEZ por thread
     def _ensure_com(self):
-        if not hasattr(self._tls, "initialized"):
+        if not hasattr(self, "initialized"):
             pythoncom.CoInitialize()
-            self._tls.uti = win.Dispatch("WinAcquisit.Utilities")
-            self._tls.emb = win.Dispatch("WinAcquisit.Embedding")
-            self._tls.emb.ShowWindow(self._tls.emb.NORMAL)
+            self.uti = win.Dispatch("WinAcquisit.Utilities")
+            self.emb = win.Dispatch("WinAcquisit.Embedding")
+            self.emb.ShowWindow(self.emb.NORMAL)
             time.sleep(2)
-            self._tls.bvt_server = win.Dispatch("WinAcquisit.BVT")
-            self._tls.initialized = True
+            self.bvt_server = win.Dispatch("WinAcquisit.BVT")
+            self.initialized = True
 
     # FINALIZA COM (chamar só no fim da thread!)
     def release_com(self):
-        if hasattr(self._tls, "initialized"):
+        if hasattr(self, "initialized"):
             try:
-                del self._tls.emb
-                del self._tls.bvt_server
-                del self._tls.uti
+                del self.emb
+                del self.bvt_server
+                del self.uti
             except:
                 pass
             pythoncom.CoUninitialize()
-            del self._tls.initialized
+            del self.initialized
 
     def start(self, gas_flow, evaporator):
-        srv = self._tls.bvt_server
+        srv = self.bvt_server
         srv.GasFlow(gas_flow)
         srv.GasFlowOn(True)
         if evaporator:
@@ -45,24 +44,24 @@ class BVT:
         return
 
     def set_point_and_start_ramp(self, temp):
-        srv = self._tls.bvt_server
+        srv = self.bvt_server
         srv.DesiredTemperature(temp)
         srv.RampGO
         return
 
     def autotune(self, switch):
-        srv = self._tls.bvt_server
+        srv = self.bvt_server
         srv.PIDTuneOn(bool(switch))
         return
 
     def get_temperature(self):
         try:
-            self.current_temp = self._tls.bvt_server.GetTemperature
+            self.current_temp = self.bvt_server.GetTemperature
         except Exception as e:
             print("ERROR READING BVT TEMPERATURES:", e)
         try:
             if self.current_temp is not None:
-                self.isTemperatureReady = bool(self._tls.bvt_server.IsTemperatureOK)
+                self.isTemperatureReady = bool(self.bvt_server.IsTemperatureOK)
         except Exception as e:
             print("ERROR CHECKING TEMPERATURE:", e)
 
